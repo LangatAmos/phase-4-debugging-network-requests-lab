6c0a79c5945886e8cd595f07e7283ac6c13d7e26
Amos Langat <amos.langat@student.moringaschool.com>
2022-11-23 22:03:33 +0300

    complete assignment

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -3,17 +3,19 @@ class ToysController < ApplicationController
 
   def index
     toys = Toy.all
-    render json: toys
+    render json: toys, status: :ok
   end
 
   def create
-    toy = Toys.create(toy_params)
+    #create a new object, should be called on model name 
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy, status: :ok
   end
 
   def destroy
@@ -22,8 +24,19 @@ class ToysController < ApplicationController
     head :no_content
   end
 
+  #update the toy by increment_likes
+  def increment_likes
+    #find the toy by id and update
+    toy = Toy.find_by(id: params[:id])
+    if toy
+      toy.update(likes: toy.likes + 1)
+      render json: toy
+    else
+      render json: { error: "Toy not found" }, status: :not_found
+    end
+  end
+
   private
-  
   def toy_params
     params.permit(:name, :image, :likes)
   end
